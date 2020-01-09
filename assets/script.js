2a490e1d6adf743c5f419da10612cc263a9567fe
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-09 16:54:39 -0500

    added time and date

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
-var nineAm = document.querySelector("9am");
-var tenAm = document.querySelector("10am");
-var elevenAm = document.querySelector("11am");
-var twelvePm = document.querySelector("12pm")
-var onePm = document.querySelector("1pm");
-var twoPm = document.querySelector("2pm");
-var threePm = document.querySelector("3pm");
-var fourPm = document.querySelector("4pm");
-var fivePm = document.querySelector("5pm");
+var nineAm = document.querySelector("nineAm");
+var tenAm = document.querySelector("tenAm");
+var elevenAm = document.querySelector("elevenAm");
+var twelvePm = document.querySelector("twelvePm")
+var onePm = document.querySelector("onePm");
+var twoPm = document.querySelector("twoPm");
+var threePm = document.querySelector("threePm");
+var fourPm = document.querySelector("fourPm");
+var fivePm = document.querySelector("fivePm");
+
+$(document).ready(function() {
+ var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+ var displayCurrentTime = $(".time-date").text(currentTime);
+ $('.time-date').css('text-align','center');
+
+});
+
