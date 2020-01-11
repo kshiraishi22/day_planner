ac6474eb2c1d57e8d5850af2fd0f297a936d9b5b
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-10 21:39:53 -0500

    added comments

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+// Define variables
+
 var nineAm = document.querySelector("nineAm");
 var tenAm = document.querySelector("tenAm");
 var elevenAm = document.querySelector("elevenAm");
@@ -8,10 +10,34 @@ var threePm = document.querySelector("threePm");
 var fourPm = document.querySelector("fourPm");
 var fivePm = document.querySelector("fivePm");
 
+// current time and date
 $(document).ready(function() {
- var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
- var displayCurrentTime = $(".time-date").text(currentTime);
- $('.time-date').css('text-align','center');
+  var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
+  var displayCurrentTime = $(".time-date").text(currentTime);
+  $(".time-date").css("text-align","center");
+
+// create an EventListener for the save buttons
+
+$(".btn9").click(function() {
+  console.log("this works!!!");
+});
 
 });
+
+// (Watch from 1:31 from Jan 3rd lecture)
+
+// Choose colors and color code the individual hrs (how to color the text area)
+
+// Create a if/then statement for the past/present/future hours to determine color
+
+
+// link save button to local storage
+
+// Align the text box where the hours are located 
+
+// Comment out to make the code more readable (html/css/js)
+
+// ReadMe file
+
+// Bonus: have a live time display with seconds ticking at the top
 
