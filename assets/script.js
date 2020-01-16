c53555f97542eaae940880a84214f8310d93361e
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-16 14:50:49 -0500

    added click button

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,54 @@
 $(document).ready(function() {
 
 // Define variables
-var currentDayELEM = $("#currentDay");
-var scheduleELEM = $("#schedule");
-var schedule = {
-  '10 am': 'Walk the dog',
-  '3 pm': 'Get groceries'
-}
+  var currentDayELEM = $("#currentDay");
+  var scheduleELEM = $("#schedule");
 
 // hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
 // Time Display
-setInterval(function () {
-  currentDayELEM.text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-}, 1 * 1000);
+  setInterval(function () {
+    currentDayELEM.text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  }, 1 * 1000);
 
 // Loop for creating rows
-for (var i = 9; i <= 17; i++) {
-  makeRow(i)
-}
+  for (var i = 9; i <= 17; i++) {
+    makeRow(i)
+  }
 
 // Function for creating rows with time, textbox and save button from 9am to 5pm and color coding based on time
-  function makeRow(hour) {
-    var now = moment();
-    var thisHours = moment(hour, "H");
-    var timeClass = "";
+    function makeRow(hour) {
+      var now = moment();
+      var thisHours = moment(hour, "H");
+      var timeClass = "";
 
-    if (now.isSame(thisHours, 'hour')) {
-      timeClass = "present";
-    } else if (now.isAfter(thisHours)) {
-      timeClass = "past";
-    } else if (now.isBefore(thisHours)) {
-      timeClass = "future";
+      if (now.isSame(thisHours, 'hour')) {
+        timeClass = "present";
+      } else if (now.isAfter(thisHours)) {
+        timeClass = "past";
+      } else if (now.isBefore(thisHours)) {
+        timeClass = "future";
+      }
+
+      var hourTXT = moment().hour(hour).format("h a");
+      var event = schedule[hourTXT];
+
+      var parent = $("<div class='row time-block'>");
+      var timeCOL = $("<div class='hour col-1'>").text(hourTXT);
+      var textCOL = $("<textarea class='description col-10'>").addClass(timeClass).text(event);
+      var saveBtn = $("<button class='saveBtn col-1'>").text("Save");
+
+      parent.append(timeCOL, textCOL, saveBtn);
+      scheduleELEM.append(parent);
     }
 
-    var hourTXT = moment().hour(hour).format("h a");
-    var event = schedule[hourTXT];
+    var textInput = document.querySelector(".description");
+    var storedInput = JSON.parse(localStorage.getItem("input")) || []; 
 
-    var parent = $("<div class='row time-block'>");
-    var timeCOL = $("<div class='hour col-1'>").text(hourTXT);
-    var textCOL = $("<textarea class='description col-10'>").addClass(timeClass).text(event);
-    var saveBtn = $("<button class='saveBtn col-1'>").text("Save");
+    var saveBtnClick = $(".saveBtn");
 
-    parent.append(timeCOL, textCOL, saveBtn);
-    scheduleELEM.append(parent);
-  }
-console.log(saveBtn);
-  // $(saveBtn).on("click", function() {
+  $(saveBtnClick).on("click", function() {
+    console.log("this works");
 
 // Comment out to make the code more readable (html/css/js)
 
@@ -54,4 +56,5 @@ console.log(saveBtn);
 
     
   
+  });
 });
