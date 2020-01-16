086e919f55976cc6e3049ddc41f094f4327fa395
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-16 09:14:45 -0500

    tutor help

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,58 @@
 // Define variables
+var currentDayELEM = $("#currentDay");
+var scheduleELEM = $("#schedule");
+var schedule = {
+  '10 am': 'Walk the dog',
+  '3 pm': 'Get groceries'
+}
 
-var nineAm = document.querySelector("#nineAm");
-var tenAm = document.querySelector("#tenAm");
-var elevenAm = document.querySelector("#elevenAm");
-var twelvePm = document.querySelector("#twelvePm")
-var onePm = document.querySelector("#onePm");
-var twoPm = document.querySelector("#twoPm");
-var threePm = document.querySelector("#threePm");
-var fourPm = document.querySelector("#fourPm");
-var fivePm = document.querySelector("#fivePm");
+// hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
+// Time Display
+setInterval(function () {
+  currentDayELEM.text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+}, 1 * 1000);
 
-var formInput_9 = document.querySelector(".formInput_9");  
+// Loop for creating rows
+for (var i = 9; i <= 17; i++) {
+  makeRow(i)
+}
 
-
-// current time and date
+// Function for creating rows with time, textbox and save button from 9am to 5pm and color coding based on time
 $(document).ready(function() {
-  var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
-  var displayCurrentTime = $(".time-date").text(currentTime);
-  $(".time-date").css("text-align","center");
+  function makeRow(hour) {
+    var now = moment();
+    var thisHours = moment(hour, "H");
+    var timeClass = "";
 
-// create an EventListener for the save buttons
-// link save button to local storage
+    if (now.isSame(thisHours, 'hour')) {
+      timeClass = "present";
+    } else if (now.isAfter(thisHours)) {
+      timeClass = "past";
+    } else if (now.isBefore(thisHours)) {
+      timeClass = "future";
+    }
 
-  renderLastRegistered();
+    var hourTXT = moment().hour(hour).format("h a");
+    var event = schedule[hourTXT];
 
-  function renderLastRegistered() {
-    for (i = 9; i < 17; i++) {
-      var saveForm_i = localStorage.getItem("saveForm_" + i);
-      $(".formInput_" + i).val(saveForm_i);
-      
-      $(".btn" + i).click(function(e) {
-        e.preventDefault();
-        var saveForm_i = $(".formInput_" + i).val();
-        localStorage.setItem("saveForm_" + i, saveForm_i);
+    var parent = $("<div class='row time-block'>");
+    var timeCOL = $("<div class='hour col-1'>").text(hourTXT);
+    var textCOL = $("<textarea class='description col-10'>").addClass(timeClass).text(event);
+    var saveBtn = $("<button class='saveBtn col-1'>").text("Save");
 
-      });
-    };
+    parent.append(timeCOL, textCOL, saveBtn);
+    scheduleELEM.append(parent);
   }
-});
+
+  $(saveBtn).on("click", function() {
 
 
 
 
-// Choose colors and color code the individual hrs (how to color the text area)
-
-// Create a if/then statement for the past/present/future hours to determine color
-
-// Align the text box where the hours are located 
 
 // Comment out to make the code more readable (html/css/js)
 
 // ReadMe file
 
-// Bonus: have a live time display with seconds ticking at the top
 
