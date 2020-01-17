f7722d230e9ed555d7e8b0aead0a50004b598848
Ken Shiraishi <kshiraishi22@gmail.com>
2020-01-17 15:08:08 -0500

    added readme content

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# hwk_5
+# Daily Planner Homework (Hwk_5)
+
+![alt text](./assets/image/daily_planner_hwk5.png)
+
+**Description**
+
+This is a calendar application that allows the user to save events for each hour of the day for a standard business day, 9am to 5pm. 
+
+Each of the time slots represents one hour and contains the following:
+
+-Time
+
+-A field to hold user input
+
+-Save button
+
+When the user provides input into a given time slot and clicks the save button, the user input and the corresponding time will be stored in localStorage. 
+
+Lastly, this application uses the Moment.js library to display at the top of the calendar, the current day and time. In addition, the library allows each hour will be color coded to reflect whether the time slot is in the past, the present, or the future.
--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,64 @@
 $(document).ready(function() {
-
-// Define variables
-  var currentDayELEM = $("#currentDay");
-  var scheduleELEM = $("#schedule");
-
-// Time Display
-  setInterval(function () {
-    currentDayELEM.text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-  }, 1 * 1000);
-
-// Loop for creating rows
-  for (var i = 9; i <= 17; i++) {
-    makeRow(i)
-  }
-
-// Function for creating rows with time, textbox and save button from 9am to 5pm and color coding based on time
-    function makeRow(hour) {
-      var now = moment();
-      var thisHours = moment(hour, "H");
-      var timeClass = "";
-
-      if (now.isSame(thisHours, 'hour')) {
-        timeClass = "present";
-      } else if (now.isAfter(thisHours)) {
-        timeClass = "past";
-      } else if (now.isBefore(thisHours)) {
-        timeClass = "future";
-      }
-
-      var hourTXT = moment().hour(hour).format("h a");
-      var event = schedule[hourTXT];
-
-      var parent = $("<div class='row time-block'>");
-      var timeCOL = $("<div class='hour col-1'>").text(hourTXT);
-      var textCOL = $("<textarea class='description col-10'>").addClass(timeClass).text(event);
-      textCOL.attr("data-attribute", i);
-      var saveBtn = $("<button class='saveBtn col-1'>").text("Save");
-
-      parent.append(timeCOL, textCOL, saveBtn);
-      scheduleELEM.append(parent);
+  // Define variables
+    var currentDayELEM = $("#currentDay");
+    var scheduleELEM = $("#schedule");
+  // Time Display
+    setInterval(function () {
+      currentDayELEM.text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    }, 1 * 1000);
+  // Loop for creating rows
+    for (var i = 9; i <= 17; i++) {
+      makeRow(i)
     }
-
-    var saveBtnClick = $(".saveBtn");
-    var textInput = $("data-attribute").val();
-
-  $(saveBtnClick).on("click", function() {
-    var formFields = JSON.stringify(localStorage.setItem("Test", textInput)); 
-    var storedInput = JSON.parse(localStorage.getItem("input")) || [];
-    console.log("this works");
-
-// Comment out to make the code more readable (html/css/js)
-
-// ReadMe file
-
-
-  
+  // Function for creating rows with time, textbox and save button from 9am to 5pm and color coding based on time
+      function makeRow(hour) {
+        var now = moment();
+        var thisHours = moment(hour, "H");
+        var timeClass = "";
+        // Controls Color Styling 
+        if (now.isSame(thisHours, 'hour')) {
+          timeClass = "present";
+        } else if (now.isAfter(thisHours)) {
+          timeClass = "past";
+        } else if (now.isBefore(thisHours)) {
+          timeClass = "future";
+        }
+        var hourTXT = moment().hour(hour).format("h a");
+        var hourID = moment().hour(hour).format("h");
+        var event = schedule[hourTXT];
+        var parent = $("<div class='row time-block'>");
+        // *** ADDED AN ID TO THE PARENT DIV *** 
+        parent.attr("id", hourID);
+        var timeCOL = $("<div class='hour col-1'>").text(hourTXT);
+        var textCOL = $("<textarea class='description col-10'>").addClass(timeClass).text(event);
+        textCOL.attr("data-attribute", i);
+        var saveBtn = $("<button class='saveBtn col-1'>").text("Save");
+        parent.append(timeCOL, textCOL, saveBtn);
+        scheduleELEM.append(parent);
+      }
+      var saveBtnClick = $(".saveBtn");
+      var textInput = $("data-attribute").val();
+    $(saveBtnClick).on("click", function() {
+      // we need to grab the specific save button associated with the proper 'data-attribute' hour <textarea> element
+      // console.log($(this).siblings()[1].val());
+      let itemVal = $(this).siblings(".description").val().trim();
+      // let itemVal = $(this).siblings(".description").data("attribute");
+      console.log(itemVal);
+      // We grab the value the user put into the textarea and use that hour data-attribute as the 'key' to put in local storage
+      let itemKey = $(this).siblings(".hour").text();
+      // let itemKey = $(this).siblings(".description").data("attribute");
+      console.log(itemKey);
+      // We store the new data into the local storage object
+      JSON.stringify(localStorage.setItem(itemKey, itemVal));
+    });
+    // *** ON LOAD *** we need to grab each hour 'key' from localStorage and populate each textarea field accordingly.
+    $("#9 .description").val(localStorage.getItem("9 am"));
+    $("#10 .description").val(localStorage.getItem("10 am"));
+    $("#11 .description").val(localStorage.getItem("11 am"));
+    $("#12 .description").val(localStorage.getItem("12 mm"));
+    $("#1 .description").val(localStorage.getItem("1 pm"));
+    $("#2 .description").val(localStorage.getItem("2 pm"));
+    $("#3 .description").val(localStorage.getItem("3 pm"));
+    $("#4 .description").val(localStorage.getItem("4 pm"));
+    $("#5 .description").val(localStorage.getItem("5 pm"));
   });
-});
